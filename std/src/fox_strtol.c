3ab9ca12f12aec0ea6607f599fbdac4469b2014b
Renard <ludovic.fernandez@epitech.eu>
2019-10-28 15:51:53 +0100

    feat(Std): Add strtol
    Tested and functional

---
+++ std/src/fox_strtol.c
@@ -0,0 +1,54 @@
+/*
+** EPITECH PROJECT, 2019
+** Libfox
+** File description:
+** Convert a numeric string into a long int
+*/
+
+#include <stdbool.h>
+#include <limits.h>
+#include "fox_define.h"
+#include "fox_string.h"
+
+__nonnull static size_t numsize(char const **s)
+{
+    size_t n = 0;
+
+    while (**s == '0')
+        *s += 1;
+    while ((*s)[n] != '\0' && CHAR_IS_NUM((*s)[n]))
+        n += 1;
+    return n;
+}
+
+__nonnull static bool get_sign(char const **s)
+{
+    bool isneg = false;
+
+    *s += fox_strcspn(*s, "+-" STR_NUMERIC);
+    while (**s != '\0' && CHAR_IS_SIGN(**s)) {
+        isneg ^= (bool) (**s == '-');
+        *s += 1;
+    }
+    return isneg;
+}
+
+__a((nonnull(1))) long fox_strtol(char const *s, char **endptr)
+{
+    ulong_t ret = 0;
+    bool isneg = get_sign(&s);
+    size_t size = numsize(&s);
+
+    if (endptr != NULL)
+        *endptr = (str_t) s + size;
+    if (size > 19)
+        return 0;
+    for (sindex_t i = 0; i < size; ) {
+        ret += CHAR_TO_N(s[i]);
+        if (++i < size)
+            ret *= 10;
+    }
+    if (ret > (ulong_t) LONG_MAX + isneg)
+        ret = 0;
+    return isneg ? (signed long) -ret : (signed long) ret;
+}
---
+++ std/tests/test_fox_strtol.c
@@ -0,0 +1,37 @@
+/*
+** EPITECH PROJECT, 2019
+** Libfox
+** File description:
+** Unit tests for fox_strtol
+*/
+
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+#include "fox_define.h"
+#include "fox_std.h"
+
+Test(fox_strtol, regular_usage)
+{
+    str_t endptr = NULL;
+    str_t num = "ptdr xD +---0123cul";
+    long res = fox_strtol(num, &endptr);
+
+    cr_expect_eq(res, -123, "res = %ld", res);
+    cr_expect_eq(endptr, num + 16);
+}
+
+Test(fox_strtol, overflow_and_underflow)
+{
+    long res = fox_strtol("-9223372036854775809", NULL);
+
+    cr_expect_eq(res, 0, "Undeflow : res = %ld", res);
+    res = fox_strtol("9223372036854775808", NULL);
+    cr_expect_eq(res, 0, "Overflow : res = %ld", res);
+}
+
+Test(fox_strtol, num_too_big)
+{
+    long res = fox_strtol("-922337203685477580913548453135487352123547", NULL);
+
+    cr_expect_eq(res, 0, "res = %ld", res);
+}
