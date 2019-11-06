eb08eb6d87fc0827bf5652509f5b4da1b964604f
Renard <ludovic.fernandez@epitech.eu>
2019-11-06 13:15:59 +0100

    feat(I/O) Add putnbr, putchar and putstr

--- extra/include/fox_io.h
+++ extra/include/fox_io.h
@@ -11,13 +11,19 @@
 #include <sys/types.h>
 #include "fox_define.h"
 
-// Print a string to stdout
-#define fox_putstr(str) fox_dputstr(1, (str))
-
-// Print a string to stderr
-#define fox_eputstr(str) fox_dputstr(2, (str))
+// Print a single character to a given file descriptor
+ssize_t fox_dputchar(int fd, char c);
+#define fox_putchar(c)  fox_dputchar(1, c) // -> STDOUT
+#define fox_eputchar(c) fox_dputchar(2, c) // -> STDERR
 
 // Print a string to a given file descriptor
 ssize_t fox_dputstr(int fd, str2c_t str);
+#define fox_putstr(str)  fox_dputstr(1, (str)) // -> STDOUT
+#define fox_eputstr(str) fox_dputstr(2, (str)) // -> STDERR
+
+// Print a number to a given file descriptor
+size_t fox_dputnbr(int fd, long long n);
+#define fox_putnbr(n)  fox_dputnbr(1, (n)) // -> STDOUT
+#define fox_eputnbr(n) fox_dputnbr(2, (n)) // -> STDERR
 
 #endif /* !FOX_IO_H */
