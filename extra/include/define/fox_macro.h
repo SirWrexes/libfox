d0836aef6ea5efd1c2e4244b2eae6226c7ead3ba
Renard <ludovic.fernandez@epitech.eu>
2019-11-18 00:31:27 +0100

    refactor/feat(Macro) Add some macros, optimize others

--- extra/include/define/fox_macro.h
+++ extra/include/define/fox_macro.h
@@ -8,22 +8,20 @@
 #ifndef FOX_MACRO_H
 #define FOX_MACRO_H
 
-/*
-** Following macros are sorted in contextual order and follow this template :
-**
-**     // What purpose the macro serves
-**     #define NAME (instruction)
-*/
+#ifndef NULL
+    #define NULL ((void *) 0)
+#endif // FOX_NULL
 
 // First we need to make sure their
 // name isn't already defined elsewhere
 #ifndef LIBFOX_MACRO_UNDEF
     #define LIBFOX_MACRO_UNDEF
     #undef __a
-    #undef __always_inline
     #undef __cleanup
     #undef __const
     #undef __fallthrough
+    #undef __format
+    #undef __leaf
     #undef __malloc
     #undef __nonnull
     #undef __packed
@@ -42,6 +40,7 @@
     #undef CHAR_IS_SIGN
     #undef CHAR_TOLOWER
     #undef CHAR_TOUPPER
+    #undef CHAR_TO_INDEX
     #undef CHAR_TO_N
     #undef IS_NULL
     #undef N_ABS
@@ -64,59 +63,42 @@
     // Shorter ways of writing attributes
     // For more information about these, check GNU GCC's docs
     // CAREFUL: These have RESTRICTED PORTABILITY
-    #define __a(attributes)    __attribute__(attributes)
-    #define __always_inline    __a((always_inline))
-    #define __fallthrough      __a((fallthrough))
-    #define __const            __a((const))
-    #define __cleanup(janitor) __a((cleanup(janitor)))
-    #define __malloc           __a((malloc))
-    #define __nonnull          __a((nonnull))
-    #define __packed           __a((packed))
-    #define __pure             __a((pure))
-    #define __transparent      __a((__transparent_union__))
-    #define __unused           __a((unused))
-
-    // Digit ascii value to numeric value
-    #define CHAR_TO_N(c) ((char) ((c) - '0'))
-
-    // Numeric value to ascii char value
-    #define N_TO_CHAR(i) ((char) (N_ABS((i)) + '0'))
+    #define __a(attributes)      __attribute__(attributes)
+    #define __cleanup(janitor)   __a((cleanup(janitor)))
+    #define __const              __a((const))
+    #define __fallthrough        __a((fallthrough))
+    #define __format(type, f, a) __a((format(type, f, a)))
+    #define __leaf               __a((leaf))
+    #define __malloc             __a((malloc))
+    #define __nonnull            __a((nonnull))
+    #define __packed             __a((packed))
+    #define __pure               __a((pure))
+    #define __transparent        __a((__transparent_union__))
+    #define __unused             __a((unused))
 
     // True if c is a printable ASCII char
-    #define CHAR_IS_PRINTABLE(c) ((c) >= 32 && (c) <= 126)
-
-    // If an alphabetical character is lowercase, make it uppercase
-    #define CHAR_TOUPPER(c)  ((c) >= 'a' && (c) <= 'z' ? (c) - 32 : (c))
-
-    // If an alphabetical character is uppercase, make it lowercase
-    #define CHAR_TOLOWER(c)  ((c) + 32 * ((c) >= 'A' && (c) <= 'Z'))
-
-    // Check if a pointer is NULL
-    #define IS_NULL(p) ((p) == NULL)
+    #define CHAR_IS_PRINTABLE(c) ((signed char) (c) - ' ' >= 0 && (c) != 127)
 
     // Check if a char is an end of line (linebreak OR null char)
     #define CHAR_IS_EOL(c) ((c) == '\n' || (c) == '\0')
 
+    // Check if a char is strictly numeric
+    #define CHAR_IS_NUM(c) ((unsigned char) (c) - '0' < 10)
+
     // Check if a char is strictly lowercase alphabetical
-    #define CHAR_IS_ALPHALO(c) ((c) >= 'a' && (c) <= 'z')
+    #define CHAR_IS_ALPHALO(c) ((unsigned char) (c) - 'a' <= 'z' - 'a')
 
     // Check if a char is strictly uppercase alphabetical
-    #define CHAR_IS_ALPHAUP(c) ((c) >= 'A' && (c) <= 'Z')
+    #define CHAR_IS_ALPHAUP(c) ((unsigned char) (c) - 'A' <= 'Z' - 'A')
 
     // Check if a char is strictly alphabetical
     #define CHAR_IS_ALPHA(c) (CHAR_IS_ALPHALO(c) || CHAR_IS_ALPHAUP(c))
 
-    // Check if a char is strictly numeric
-    #define CHAR_IS_NUM(c) ((unsigned char) (c) - '0' < 10)
-
-    // Check if a number is pair
-    #define N_IS_EVEN(n) !N_IS_ODD(n)
-
-    // Check if a number is odd
-    #define N_IS_ODD(n) ((n) % 2)
-
     // Check if a char is strictly alphanumeric
     #define CHAR_IS_ALPHANUM(c) (CHAR_IS_ALPHA(c) || CHAR_IS_NUM(c))
+
+    // Check if a char is a sign (+ OR -)
+    #define CHAR_IS_SIGN(c) ((c) == '+' || (c) == '-')
 
     // Check if a char is punctuation
     #define CHAR_IS_PUNCT(c)    \
@@ -135,8 +117,17 @@
         || ((c) == '/')         \
     )                           \
 
-    // Check if a char is a sign (+ OR -)
-    #define CHAR_IS_SIGN(c) ((c) == '+' || (c) == '-')
+    // If an alphabetical character is lowercase, make it uppercase
+    #define CHAR_TOUPPER(c)  ((c) - 40 * CHAR_IS_ALPHALO((c)))
+
+    // If an alphabetical character is uppercase, make it lowercase
+    #define CHAR_TOLOWER(c)  ((c) + 40 * CHAR_IS_ALPHAUP((c)))
+
+    // Digit ascii value to numeric value
+    #define CHAR_TO_N(c) ((char) ((c) - '0'))
+
+    // Numeric value to ascii char value
+    #define N_TO_CHAR(i) ((char) ((i) + '0'))
 
     // Get the absolute value of a number
     #define N_ABS(x) ((unsigned) ((x) >= 0 ? (x) : (x) * -1))
@@ -144,13 +135,18 @@
     // Get the biggest of two numbers
     #define N_MAX(x, y) ((x) > (y) ? (x) : (y))
 
+    // Check if a number is pair
+    #define N_IS_EVEN(n) !N_IS_ODD(n)
+
+    // Check if a number is odd
+    #define N_IS_ODD(n) ((n) % 2)
+
+    // Check if a pointer is NULL
+    #define IS_NULL(p) ((p) == NULL)
+
     // One-liner to return value and execute any function(s)
     // -- I.G. returning a specific value while printing an error string
     #define RETURN(value, funcs...) return (0 ? : (funcs, value))
 #endif // LIBFOX_MACRO_DEF
-
-#ifndef NULL
-    #define NULL ((void *) 0)
-#endif // FOX_NULL
 
 #endif /* !FOX_MACRO_H */
