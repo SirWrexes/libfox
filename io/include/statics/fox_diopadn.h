6dc8d0a140461c0015b767e677d9d36cc3249ac1
Renard <ludovic.fernandez@epitech.eu>
2019-11-18 23:23:12 +0100

    feat(IO) Add statics header folder
    This folder will contain stuff that are private to some functions. Those headers will have
    the same name as the source they are included in.

---
+++ io/include/statics/fox_diopadn.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2019
+** Libfox
+** File description:
+** Private header for diopadn
+*/
+
+#ifndef FOX_DIOPADN_H
+#define FOX_DIOPADN_H
+
+#define PADSIZE 16
+static const char BLANKS[] = {[0 ... (PADSIZE - 1)] = ' '};
+static const char ZEROES[] = {[0 ... (PADSIZE - 1)] = '0'};
+
+#endif /* !FOX_DIOPADN_H */
