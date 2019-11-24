e5adb21a3a86d7a82b45e42b467b83b1f9f0739d
Renard <ludovic.fernandez@epitech.eu>
2019-11-24 15:48:28 +0100

    refactor(IO) Make set_padbuff a static inline

--- io/include/statics/fox_diopadn.h
+++ io/include/statics/fox_diopadn.h
@@ -8,8 +8,17 @@
 #ifndef FOX_DIOPADN_H
 #define FOX_DIOPADN_H
 
+#include "define/fox_types.h"
+
 #define PADSIZE 16
 static const char BLANKS[] = {[0 ... (PADSIZE - 1)] = ' '};
 static const char ZEROES[] = {[0 ... (PADSIZE - 1)] = '0'};
+
+static inline str_t set_padbuff(char padbuff[PADSIZE], uchar_t pad)
+{
+    for (hindex_t i = 0; i < PADSIZE; i += 1)
+        padbuff[i] = pad;
+    return padbuff;
+}
 
 #endif /* !FOX_DIOPADN_H */
