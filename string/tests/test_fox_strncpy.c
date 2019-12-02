6805e0caa902d01dac2654141a44d2dd6ece2cab
Renard <ludovic.fernandez@epitech.eu>
2019-12-02 22:37:06 +0100

    refactor(String) Adapt fox_strncpy tests to new version of the function
    fox_strncpy now reproduces the original strncpy's behaviour exactly; thus meaning that if the first
    n bytes of src you copy into dst don't have a null terminater, dst won't have one either.

--- string/tests/test_fox_strncpy.c
+++ string/tests/test_fox_strncpy.c
@@ -15,7 +15,7 @@ Test(fox_strncpy, n_gt_src_len)
     char dest[30] = "You want the truth ?";
     char src[30] = "You can't handle the truth !";
 
-    cr_expect_str_eq(fox_strncpy(dest, src, 50), src);
+    cr_expect_arr_eq(fox_strncpy(dest, src, 50), src, strlen(src));
 }
 
 Test(fox_strncpy, n_lt_src_len)
@@ -23,7 +23,7 @@ Test(fox_strncpy, n_lt_src_len)
     char dest[30] = "You want the truth ?";
     char src[30] = "You can't handle the truth !";
 
-    cr_expect_str_eq(fox_strncpy(dest, src, 9), "You can't");
+    cr_expect_arr_eq(fox_strncpy(dest, src, 9), src, 9);
 }
 
 Test(fox_strncpy, n_lt_dest_len)
@@ -31,7 +31,7 @@ Test(fox_strncpy, n_lt_dest_len)
     char dest[30] = "Cheese Harissa";
     char src[30] = "Poivre";
 
-    cr_expect_str_eq(fox_strncpy(dest, src, 6), "Poivre");
+    cr_expect_str_eq(fox_strncpy(dest, src, 7), "Poivre");
 }
 
 Test(fox_strncpy, n_lt_both_len)
