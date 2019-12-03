ee2a66d944eb391bb8af796a2e207a5932a05ad2
Renard <ludovic.fernandez@epitech.eu>
2019-12-03 17:11:54 +0100

    feat(Wrappers) Add wrappers for close and write

--- extra/makeinclude/wrappers.mk
+++ extra/makeinclude/wrappers.mk
@@ -21,15 +21,28 @@ WRAPSRC += ../extra/tests/wrappers/wrap_open.c
 WRAPPED += open
 #############################################################
 
+# Close
+#############################################################
+WRAPSRC += ../extra/tests/wrappers/wrap_close.c
+WRAPPED += close
+#############################################################
+
 # Read
 #############################################################
 WRAPSRC += ../extra/tests/wrappers/wrap_read.c
 WRAPPED += read
 #############################################################
 
+# Write
+#############################################################
+WRAPSRC += ../extra/tests/wrappers/wrap_write.c
+WRAPPED += write
+#############################################################
+
 # Wrap flags for compilation
 #############################################################
-WRAPFLAGS   := -Wl$(foreach wrap, $(WRAPPED),,--wrap=$(wrap))
+WRAPPREFIX := -Wl,--wrap=
+WRAPFLAGS  := $(addprefix $(WRAPPREFIX),$(WRAPPED))
 #############################################################
 
 endif
