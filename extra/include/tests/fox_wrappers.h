12e445fcfcc080983e09412527872e76c69545dc
Renard <ludovic.fernandez@epitech.eu>
2019-12-03 17:08:31 +0100

    refactor(Wrappers) Move individual wrapper headers to their own dir

---
+++ extra/include/tests/fox_wrappers.h
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2019
+** Libfox
+** File description:
+** Include every wrapper
+*/
+
+#ifndef FOX_WRAPPERS_H
+#define FOX_WRAPPERS_H
+
+#include "wrappers/wrap_open.h"
+#include "wrappers/wrap_close.h"
+#include "wrappers/wrap_read.h"
+#include "wrappers/wrap_write.h"
+#include "wrappers/wrap_malloc.h"
+
+#endif /* !FOX_WRAPPERS_H */
