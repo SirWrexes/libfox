83c081d1a84b9d63a859afc5b6b6070c29e5d641
Renard <ludovic.fernandez@epitech.eu>
2019-12-05 19:16:40 +0100

    style(Wrappers) Separate and sort includes in fox_wrappers.h

--- extra/include/tests/fox_wrappers.h
+++ extra/include/tests/fox_wrappers.h
@@ -8,10 +8,19 @@
 #ifndef FOX_WRAPPERS_H
 #define FOX_WRAPPERS_H
 
-#include "wrappers/wrap_open.h"
+/* FCNTL / UNISTD */
 #include "wrappers/wrap_close.h"
+#include "wrappers/wrap_open.h"
 #include "wrappers/wrap_read.h"
 #include "wrappers/wrap_write.h"
+
+/* MALLOC */
 #include "wrappers/wrap_malloc.h"
+
+/* NCURSES */
+#include "wrappers/wrap_cbreak.h"
+#include "wrappers/wrap_initscr.h"
+#include "wrappers/wrap_keypad.h"
+#include "wrappers/wrap_noecho.h"
 
 #endif /* !FOX_WRAPPERS_H */
