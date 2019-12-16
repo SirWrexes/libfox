e9b8df30a504d10a637b53d438ecfffcd4079308
Renard <ludovic.fernandez@epitech.eu>
2019-12-16 22:47:11 +0100

    build(CSFML) Add csfml-graphics lib inclusion

--- csfml/makeinclude/module.mk
+++ csfml/makeinclude/module.mk
@@ -19,13 +19,17 @@ NAME := csfml
 ################################################
 DEBUGMAIN := ./src/debugmain.c
 # -- Animation tools
-SRC := ./src/animinfo/ai_on_last_frame.c
-SRC += ./src/animinfo/ai_scale_current.c
-SRC += ./src/animinfo/ai_rect_current.c
-SRC += ./src/animinfo/ai_rect_first.c
-SRC += ./src/animinfo/ai_rect_next.c
-SRC += ./src/animinfo/ai_rect_prev.c
-SRC += ./src/animinfo/ai_type.c
+SRC := ./src/animation/ai_on_last_frame.c
+SRC += ./src/animation/ai_scale_current.c
+SRC += ./src/animation/ai_rect_current.c
+SRC += ./src/animation/ai_rect_first.c
+SRC += ./src/animation/ai_rect_next.c
+SRC += ./src/animation/ai_rect_prev.c
+SRC += ./src/animation/ai_type.c
+# -- Sprite tools
+SRC += ./src/sprite/si_update_scale.c
+SRC += ./src/sprite/si_update_pos.c
+SRC += ./src/sprite/si_update_pos_info.c
 ################################################
 
 
@@ -33,13 +37,13 @@ SRC += ./src/animinfo/ai_type.c
 # Tests (set relative to master Makefile path)
 ################################################
 # -- Animation tools
-TST := ./tests/animinfo/test_ai_on_last_frame.c
-TST += ./tests/animinfo/test_ai_scale_current.c
-TST += ./tests/animinfo/test_ai_rect_current.c
-TST += ./tests/animinfo/test_ai_rect_first.c
-TST += ./tests/animinfo/test_ai_rect_next.c
-TST += ./tests/animinfo/test_ai_rect_prev.c
-TST += ./tests/animinfo/test_ai_type.c
+TST := ./tests/animation/test_ai_on_last_frame.c
+TST += ./tests/animation/test_ai_scale_current.c
+TST += ./tests/animation/test_ai_rect_current.c
+TST += ./tests/animation/test_ai_rect_first.c
+TST += ./tests/animation/test_ai_rect_next.c
+TST += ./tests/animation/test_ai_rect_prev.c
+TST += ./tests/animation/test_ai_type.c
 ################################################
 
 
@@ -48,6 +52,14 @@ TST += ./tests/animinfo/test_ai_type.c
 ################################################
 DEPSRC :=
 DEPSRC +=
+################################################
+
+
+#
+# Additional libs
+################################################
+LIBS := csfml-graphics
+LIBS +=
 ################################################
 
 endif
