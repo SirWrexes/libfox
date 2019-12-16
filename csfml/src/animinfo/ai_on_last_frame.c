a5930f9a63d94a9beec7705c4bb2af7e5d0c8c12
Renard <ludovic.fernandez@epitech.eu>
2019-12-16 20:14:55 +0100

    refactor(CSMLF) Make ai_on_last_frame be just one inline switch 7function

--- csfml/src/animinfo/ai_on_last_frame.c
+++ csfml/src/animinfo/ai_on_last_frame.c
@@ -6,18 +6,16 @@
 */
 
 #include "fox_define.h"
-
-#include "animinfo.h"
-#include "datastruct.h"
+#include "fox_csfml.h"
 
 __Anonnull __AalwaysILext bool ai_on_last_frame(animinfo_t ai)
 {
-    switch (ai->types[ai->current]) {
+    switch (ai->type[ai->current]) {
         case ANIM_TYPE_ONCE:
-        case ANIM_TYPE_LOOP: return ai_next_rect(ai)->top == -1;
+        case ANIM_TYPE_LOOP: return ai_rect_next(ai)->top == -1;
         case ANIM_TYPE_PING_PONG:
             return (ai->pongstep == PP_BACKWARD && ai->frame == 0)
-                || (ai->pongstep == PP_FORWARD && ai_next_rect(ai)->top == 0);
+                || (ai->pongstep == PP_FORWARD && ai_rect_next(ai)->top == -1);
         case ANIM_TYPE_NONE:
         default: return true;
     }
