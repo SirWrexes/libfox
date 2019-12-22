651ef40d49bc5db60d95292efb7b63dd939ed855
Renard <ludovic.fernandez@epitech.eu>
2019-12-22 15:45:14 +0100

    perf(Memory) Get rid of temp variables in memcpy

--- memory/src/fox_memcpy.c
+++ memory/src/fox_memcpy.c
@@ -16,21 +16,16 @@ __a((nonnull(2))) void *fox_memcpy(void *dst, const void *src, size_t n)
     ulong_t *dstp = (ulong_t *) dst;
     byte_t *srcbp;
     byte_t *dstbp;
-    ulong_t word;
-    byte_t byte;
 
     if (dst != NULL) {
         while (n / ULSIZE != 0) {
-            word = *srcp++;
-            *dstp++ = word;
+            *dstp++ = *srcp++;
             n -= ULSIZE;
         }
         srcbp = (byte_t *) srcp;
         dstbp = (byte_t *) dstp;
-        while (n-- != 0) {
-            byte = *srcbp++;
-            *dstbp++ = byte;
-        }
+        while (n-- != 0)
+            *dstbp++ = *srcbp++;
     }
     return dst;
 }
