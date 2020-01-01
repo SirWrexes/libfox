a4f5962278c94afc96ef58f95c337b8af437d39d
Renard <ludovic.fernandez@epitech.eu>
2020-01-02 00:52:48 +0100

    feat/refactor(CSFML) Add textureinfo_t, edit spinfo_t
    texutreinfo_t (struct texture_info) contains data about a texutre. Don't use it directly, use the
    foxTexture_add() macro.
    
    spinfo_t changes:
      - isolate entid on top of struct deifinition for clarity reasons
      - add **texture, which contains a pointer to the texutre's pointer (initialized with foxTexture_add() macro)
      - make animinfo a struct anim_info pointer to pointer array instead of pointer flexible array

--- csfml/include/types/foxcsfml_sprite.h
+++ csfml/include/types/foxcsfml_sprite.h
@@ -11,17 +11,28 @@
 #include <SFML/Graphics/Types.h>
 #include <SFML/System/Vector2.h>
 
+#include "define/fox_types.h"
+
 #include "foxcsfml_animation.h"
 
 // Sprite object
 typedef struct sprite_info {
-    unsigned entid;      // Type of the entity the sprite is attached to
-    sfSprite *sprite;    // Sprite itself
-    sfVector2f position; // Sprite starting position
-    sfVector2f origin;   // Sprite center
+    unsigned entid; // Type of the entity the sprite is attached to
 
-    unsigned curranim;                // Current animation
-    struct animation_info animinfo[]; // Animation info array
+    sfSprite *sprite;          // Sprite itself
+    sfVector2f position;       // Sprite starting position
+    sfVector2f origin;         // Sprite center
+    const sfTexture **texture; // Sprite texture
+
+    unsigned curranim;                      // Current animation
+    unsigned maxanims;                      // Maximum animation count
+    const struct animation_info **animinfo; // Animation info pointer array
 } * spinfo_t;
+
+// Texture informatition
+typedef const struct texture_info {
+    const str_t path;     // Texutre path
+    const sfIntRect area; // Texture area
+} textureinfo_t;
 
 #endif /* !FOXCSFML_SPRITE_H */
