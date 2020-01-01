83f5209f8ced2f89182b6f197cc0c41c30f3da5c
Renard <ludovic.fernandez@epitech.eu>
2020-01-02 00:39:25 +0100

    refactor(CSFML) Rename 'internal' to 'macro'

---
+++ csfml/include/macro/foxcsfml_macro.h
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2019
+** Libfox
+** File description:
+** Fox CSFML macro collection
+*/
+
+#ifndef FOXCSFML_MACRO_H
+#define FOXCSFML_MACRO_H
+
+#include "macro/foxcsfml_texture.h"
+#include "macro/foxcsfml_tick.h"
+
+#endif /* !FOXCSFML_MACRO_H */
