5132e7e97c9f01e82da688e5189f7624d510481a
Renard <ludovic.fernandez@epitech.eu>
2020-01-03 14:04:42 +0100

    WIP: Try fixing github workflow
    Locate missing files

--- .github/workflows/build_csfml.sh
+++ .github/workflows/build_csfml.sh
@@ -5,38 +5,41 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-SFML_SOURCE_URL="http://www.sfml-dev.org/files/SFML-2.5.1-sources.zip"
+# SFML_SOURCE_URL="http://www.sfml-dev.org/files/SFML-2.5.1-sources.zip"
 CSFML_SOURCE_URL="http://www.sfml-dev.org/files/CSFML-2.5-sources.zip"
 
 CSFML_ZIP="CSFML.zip"
-SFML_ZIP="SFML.zip"
+# SFML_ZIP="SFML.zip"
 
-echo "Download SFML Sources"
-curl -Lo "$SFML_ZIP" $SFML_SOURCE_URL
+# echo "Download SFML Sources"
+# curl -Lo "$SFML_ZIP" $SFML_SOURCE_URL
 echo "Download CSFML Sources"
 curl -Lo "$CSFML_ZIP" $CSFML_SOURCE_URL
 
-echo "Unzip SFML"
-unzip -qq -o $SFML_ZIP
+# echo "Unzip SFML"
+# unzip -qq -o $SFML_ZIP
 echo "Unzip CSFML"
 unzip -qq -o $CSFML_ZIP
 
-mv SFML-* SFML
+# mv SFML-* SFML
 mv CSFML-* CSFML
 
-SFML_PATH="$(realpath SFML)"
+# SFML_PATH="$(realpath SFML)"
 CSFML_PATH="$(realpath CSFML)"
 
-echo "SFML Compilation"
-cd SFML
-cmake .
-make
-cd ..
+# echo "SFML Compilation"
+# cd SFML
+# cmake .
+# make
+# cd ..
+
+find . -depth \( -name "SFMLConfig.cmake" -o -name "sfml-config.cmake" -o -name "FindSFML.cmake" \)
 
 echo "CSFML Compilation"
 cd CSFML
-cmake -DSFML_ROOT="$SFML_PATH" -DSFML_INCLUDE_DIR="$SFML_PATH/include" -DCMAKE_MODULE_PATH="$SFML_PATH/cmake/Modules" .
-LD_LIBRARY_PATH="$SFML_PATH/lib"
+cmake .
+# ↑ -DSFML_ROOT="$SFML_PATH" -DSFML_INCLUDE_DIR="$SFML_PATH/include" -DCMAKE_MODULE_PATH="$SFML_PATH/cmake/Modules" .
+#LD_LIBRARY_PATH="$SFML_PATH/lib"
 make
 make install
 cd ..
