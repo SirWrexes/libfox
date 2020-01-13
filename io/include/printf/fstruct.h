62e580cfb31d4611c731941956b8d8838f5b2e26
Renard <ludovic.fernandez@epitech.eu>
2020-01-13 10:14:33 +0100

    style(IO) Reformat printf/fstruct.h

--- io/include/printf/fstruct.h
+++ io/include/printf/fstruct.h
@@ -8,23 +8,23 @@
 #ifndef ARGS_H
 #define ARGS_H
 
-#include <stdbool.h>
 #include <stdarg.h>
+#include <stdbool.h>
 #include <sys/types.h>
 #include "fox_define.h"
 
 typedef struct format_struct fstruct_t;
 #ifndef CONVERTER_TYPE_DEFINED
     #define CONVERTER_TYPE_DEFINED
-    typedef scount_t (*converter_t)(fstruct_t *, va_list *);
+typedef scount_t (*converter_t)(fstruct_t *, va_list *);
 #endif // CONVERTER_TYPE_DEFINED
 
 typedef struct {
-    unsigned prec;           // Precision
-    unsigned width;          // Field width
-    char padchar;            // Field fill char ('0'/' ' flags)
+    unsigned prec;  // Precision
+    unsigned width; // Field width
+    char padchar;   // Field fill char ('0'/' ' flags)
 
-    char spec;               // Format letter
+    char spec; // Format letter
 
     bool is_char : 1;        // hh length modifier
     bool is_short : 1;       // h length modifier
@@ -32,18 +32,18 @@ typedef struct {
     bool is_long_long : 1;   // ll length modifier
     bool is_long_double : 1; // L length modifier
 
-    bool alt : 1;            // # flag
-    bool group : 1;          // ' flag
-    bool left : 1;           // - flag
-    bool showsign : 1;       // + flag
-    bool space : 1;          // Space flag
+    bool alt : 1;      // # flag
+    bool group : 1;    // ' flag
+    bool left : 1;     // - flag
+    bool showsign : 1; // + flag
+    bool space : 1;    // Space flag
 
-    bool is_valid : 1;       // For invalid formats
+    bool is_valid : 1; // For invalid formats
 
-    const int bfpad : 5;     // Bit field padding
+    const int bfpad : 5; // Bit field padding
 
-    ushort_t infomask;       // Mask containgin all the previous info
-} finfo_t; // Flags, field width, conversion specifier etc.
+    ushort_t infomask; // Mask containgin all the previous info
+} finfo_t;             // Flags, field width, conversion specifier etc.
 
 typedef union {
     char av_char;       // hhi, hhd
@@ -61,7 +61,7 @@ typedef union {
     ullong_t av_ullong; // llu, llb, llo, llx, llX
     str2c_t av_str;     // s, S
     void *av_ptr;       // p, P
-} fargv_t; // Format argument value
+} fargv_t;              // Format argument value
 
 struct format_struct {
     finfo_t info;      // Info from "%<...>"
