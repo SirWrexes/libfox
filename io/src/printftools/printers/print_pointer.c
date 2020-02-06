b79c3aee47af0a9fa0f627a6fb5c58c1668f5280
Renard <ludovic.fernandez@epitech.eu>
2020-02-06 12:58:28 +0100

    refactor/fix(IO:Printf) Cleanup print_pointer.c

--- io/src/printftools/printers/print_pointer.c
+++ io/src/printftools/printers/print_pointer.c
@@ -7,11 +7,12 @@
 
 #include "printf/fstruct.h"
 #include "printf/printers.h"
+#include "printf/infomask.h"
 
 __Anonnull __AalwaysILext scount_t print_pointer(fstruct_t *arg, va_list *va)
 {
     arg->info.alt = true;
-    arg->info.is_long = true;
-    arg->info.prec = arg->info.prec == 'p' ? 'x' : 'X';
+    arg->info.is_long_long = !(info_to_mask(&arg->info) & MASK_TYPE);
+    arg->info.spec += ('x' - 'p');
     return print_unsigned_base(arg, va);
 }
