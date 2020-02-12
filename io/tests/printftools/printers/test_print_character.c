291b3a40175002fa757a7834f58a218f8ee0f835
Renard <ludovic.fernandez@epitech.eu>
2020-02-12 13:34:08 +0100

    tests(IO:Printf) Field width support for %c

--- io/tests/printftools/printers/test_print_character.c
+++ io/tests/printftools/printers/test_print_character.c
@@ -25,3 +25,30 @@ Test(print_character, regular_usage, .init = cr_redirect_stdout)
     cr_expect_stdout_eq_str("c");
     va_end(ap);
 }
+
+Test(print_character, right_padding, .init = cr_redirect_stdout)
+{
+    va_list ap;
+    fstruct_t arg = {0};
+    char ref = 'c';
+
+    setup_va_list(&ap, ref);
+    arg.info.width = 5;
+    cr_assert_eq(print_character(&arg, &ap), 5);
+    cr_expect_stdout_eq_str("    c");
+    va_end(ap);
+}
+
+Test(print_character, left_padding, .init = cr_redirect_stdout)
+{
+    va_list ap;
+    fstruct_t arg = {0};
+    char ref = 'c';
+
+    setup_va_list(&ap, ref);
+    arg.info.width = 5;
+    arg.info.left = true;
+    cr_assert_eq(print_character(&arg, &ap), 5);
+    cr_expect_stdout_eq_str("c    ");
+    va_end(ap);
+}
