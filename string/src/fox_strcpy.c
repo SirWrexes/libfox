bf6e5e4feee1dd2c104efccb5901a4fc2ba754b6
Renard <ludovic.fernandez@epitech.eu>
2020-02-16 21:55:23 +0100

    refactor(strcpy) Use __AalwaysILext

--- string/src/fox_strcpy.c
+++ string/src/fox_strcpy.c
@@ -9,7 +9,7 @@
 #include "fox_memory.h"
 #include "fox_string.h"
 
-__a((nonnull(2))) extern inline str_t fox_strcpy(str_t dst, str2c_t src)
+__a((nonnull(2))) __AalwaysILext str_t fox_strcpy(str_t dst, str2c_t src)
 {
     return fox_memcpy(dst, src, fox_strlen(src) + 1);
 }
