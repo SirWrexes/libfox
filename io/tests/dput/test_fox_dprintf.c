901575e14a2eb41098ebf8be81ecb8dce84c5bed
Renard <ludovic.fernandez@epitech.eu>
2020-02-16 12:42:07 +0100

    test(Printf) Add test that failed in school tests
    Working now and corresponding issue is fixed.

---
+++ io/tests/dput/test_fox_dprintf.c
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2020
+** Libfox
+** File description:
+** test_fox_dprintf.c -- No description
+*/
+
+#define _GNU_SOURCE
+#include <stdio.h>
+
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+
+#include "fox_io.h"
+
+Test(dprintf, mixed_string_and_number, .init = cr_redirect_stdout)
+{
+    str_t ref = NULL;
+    str2c_t s = "Astek";
+    int d = 42;
+
+    cr_expect_eq(fox_printf("%%s%%d%s%d%%\n", s, d),
+        asprintf(&ref, "%%s%%d%s%d%%\n", s, d));
+    cr_expect_stdout_eq_str(ref);
+}
